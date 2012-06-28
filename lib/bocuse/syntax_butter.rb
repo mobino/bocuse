91dd70cff17bd22c0b8237b1f10b2a83bae93732
Florian Hanke <florian.hanke@gmail.com>
2012-06-28 11:40:24 +1000

    + SyntaxButter syntax sugar module layout

---
+++ lib/bocuse/syntax_butter.rb
@@ -0,0 +1,20 @@
+module Bocuse
+
+  # Bocuse would never use syntax sugar, but
+  # instead uses syntax butter.
+  #
+  # Features that are about building the
+  # hash are the core of bocuse.
+  #
+  # Features that are Chef related, are
+  # bundled here.
+  #
+  # For example, the "cook" declaration,
+  # application definitions, words like
+  # "node", or "template" are found here.
+  #
+  module SyntaxButter
+    
+  end
+
+end
