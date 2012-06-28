d89c2c56768f266311dcb18e8c48510ed55b9bab
Florian Hanke <florian.hanke@gmail.com>
2012-06-28 13:28:45 +1000

    + load the right files

--- lib/bocuse.rb
+++ lib/bocuse.rb
@@ -2,3 +2,7 @@ require 'active_support'
 
 require File.expand_path '../bocuse/value', __FILE__
 require File.expand_path '../bocuse/configuration', __FILE__
+require File.expand_path '../bocuse/syntax_butter', __FILE__
+require File.expand_path '../bocuse/templates', __FILE__
+require File.expand_path '../bocuse/file', __FILE__
+require File.expand_path '../bocuse/loader', __FILE__
