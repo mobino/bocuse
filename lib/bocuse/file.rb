ced57a3126272727085f65e30344469b9a2daf10
Kaspar Schiess <kaspar.schiess@technologyastronauts.ch>
2012-07-02 12:54:18 +0200

    + Different def context for Bocuse::File
    If we define the module around this, we'll have a lookup context of
    Bocuse for all files evaled here - it is convenient for the user to
    still be able to do File.dirname without worrying about Bocuse.

--- lib/bocuse/file.rb
+++ lib/bocuse/file.rb
@@ -1,92 +1,88 @@
-module Bocuse
+# Represents a bocuse File.
+#
+# This is usually a file that contains one of the
+# following directives:
+#  * node
+#  * template
+#
+# It is used to generate a config hash from the given files.
+#
+class Bocuse::File
+      
+  def __configuration__
+    @configuration ||= Bocuse::Configuration.new
+  end
   
-  # Represents a bocuse File.
+  # Returns a config hash.
   #
-  # This is usually a file that contains one of the
-  # following directives:
-  #  * node
-  #  * template
+  # Call to_h on the returned configuration to
+  # get the config hash.
   #
-  # It is used to generate a config hash from the given files.
+  # Will return nil if there is no configuration
+  # to speak of inside the file. Use "node" or
+  # "template" to define a configuration.
   #
-  class File
-        
-    def __configuration__
-      @configuration ||= Configuration.new
+  def evaluate filename
+    ::File.open filename, 'r' do |file|
+      self.instance_eval file.read, file.path
     end
+    __configuration__
+  end
+  
+  # Evaluates all files.
+  #
+  # Retrieve the found configurations via
+  #   Nodes.find pattern_or_name
+  #
+  # @param dir [String] directory to use as project base directory
+  # @return [void]
+  #
+  def self.evaluate_all dir=nil
+    project_path = dir || ::File.expand_path('config', Dir.pwd)
+    nodes_path = ::File.join(project_path, 'nodes')
     
-    # Returns a config hash.
-    #
-    # Call to_h on the returned configuration to
-    # get the config hash.
-    #
-    # Will return nil if there is no configuration
-    # to speak of inside the file. Use "node" or
-    # "template" to define a configuration.
-    #
-    def evaluate filename
-      ::File.open filename, 'r' do |file|
-        self.instance_eval file.read, file.path
-      end
-      __configuration__
-    end
+    # Make sure that project libraries can be loaded: 
+    $:.unshift ::File.join(project_path, 'lib')
     
-    # Evaluates all files.
-    #
-    # Retrieve the found configurations via
-    #   Nodes.find pattern_or_name
-    #
-    # @param dir [String] directory to use as project base directory
-    # @return [void]
-    #
-    def self.evaluate_all dir=nil
-      project_path = dir || ::File.expand_path('config', Dir.pwd)
-      nodes_path = ::File.join(project_path, 'nodes')
-      
-      # Make sure that project libraries can be loaded: 
-      $:.unshift ::File.join(project_path, 'lib')
-      
-      Dir[::File.join(nodes_path, '**', '*.rb')].each do |filename|
-        new.evaluate filename
-      end
+    Dir[::File.join(nodes_path, '**', '*.rb')].each do |filename|
+      new.evaluate filename
     end
+  end
+  
+  # Include the given template name.
+  #
+  # Note: This could be pushed to the configuration.
+  #
+  def include_template identifier
+    template = Bocuse::Templates.get identifier.to_sym
     
-    # Include the given template name.
+    # Note: Template is dupped to avoid evaluating the original.
     #
-    # Note: This could be pushed to the configuration.
-    #
-    def include_template identifier
-      template = Templates.get identifier.to_sym
-      
-      # Note: Template is dupped to avoid evaluating the original.
-      #
-      __configuration__.merge! template.dup
-    end
-    
-    # Make the given module a helper module for this node. 
-    #
-    def helper_module(mod)
-      extend mod
-    end
-    
-    # The files read by #evaluate will trigger these methods.
-    #
-    def node name = nil
-      yield __configuration__
-      Nodes.put name, __configuration__ # Note: Cache this node.
-      __configuration__
-    end
-    def template name = nil
-      yield __configuration__
-      __configuration__
-    end
-    
-    # Cook adds to the toplevel recipes of this file's configuration.
-    #
-    def cook recipe
-      __configuration__.recipes << recipe
-    end
-    
+    __configuration__.merge! template.dup
+  end
+  
+  # Make the given module a helper module for this node. 
+  #
+  def helper_module(mod)
+    extend mod
+  end
+  
+  # The files read by #evaluate will trigger these methods.
+  #
+  def node name = nil
+    yield __configuration__
+    Bocuse::Nodes.put name, __configuration__ # Note: Cache this node.
+    __configuration__
+  end
+  def template name = nil
+    yield __configuration__
+    __configuration__
+  end
+  
+  # Cook adds to the toplevel recipes of this file's configuration.
+  #
+  def cook recipe
+    __configuration__.recipes << recipe
   end
   
 end
