093016570609307115bd03846248a05970afee56
Kaspar Schiess <kaspar.schiess@technologyastronauts.ch>
2012-09-11 13:58:17 +0200

    . fixes spec (oversight...)

--- spec/integration/cli_spec.rb
+++ spec/integration/cli_spec.rb
@@ -10,7 +10,7 @@ describe "CLI:" do
       compiled = MultiJson.load(`#{project_path('bin/bocuse')} \
         compile complex.production.example.com`)
       compiled.size.should == 3
-      compiled['recipes'].should == ['nginx', 'git', 'app::install', 'app::deploy'];
+      compiled['recipes'].should == ['nginx', 'git', 'app::install', 'app::deploy', 'my_service'];
       compiled['key_location'].should match(%r{spec/files/complex/config/nodes/production/key.txt$})
     end
   end
