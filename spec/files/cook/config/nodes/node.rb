8616612bfc2158ed13e4ec0222aad3e28157ac01
Kaspar Schiess <kaspar.schiess@technologyastronauts.ch>
2012-11-08 15:48:44 +0100

    Issue: When a hash key is named 'test', everything blows up

---
+++ spec/files/cook/config/nodes/node.rb
@@ -0,0 +1,3 @@
+node 'node' do |cfg|
+  cook 'test'
+end
---
+++ spec/integration/cook_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+
+describe "bocuse environment" do
+  let(:project) { Bocuse::Project.new(fixture('cook')) }
+    
+  it "exports node_name" do
+    pending "Open issue" do
+      expect { 
+        project.nodes['node'].to_h
+      }.not_to raise_error
+    end
+  end
+end
